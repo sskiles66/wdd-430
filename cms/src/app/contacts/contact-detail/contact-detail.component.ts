f4521a5638c6a527e28e17a4bb57261ee48cfa23
Stephen Skiles <stephentskiles@hotmail.com>
2024-09-24 14:54:47 -0600

    Finished assigment 2

---
+++ cms/src/app/contacts/contact-detail/contact-detail.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { Contact } from '../contact.model';
+
+@Component({
+  selector: 'app-contact-detail',
+  templateUrl: './contact-detail.component.html',
+  styleUrl: './contact-detail.component.css'
+})
+export class ContactDetailComponent {
+  contact: Contact;
+}
---
+++ cms/src/app/contacts/contact-list/contact-list.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+import { Contact } from '../contact.model';
+
+@Component({
+  selector: 'app-contact-list',
+  templateUrl: './contact-list.component.html',
+  styleUrl: './contact-list.component.css'
+})
+export class ContactListComponent {
+  contacts: Contact[] = [
+    {
+      id: "1",
+      name: "R. Kent Jackson",
+      email: "jacksonk@byui.edu",
+      phone: "208-496-3771",
+      imageUrl: "jacksonk.jpg",
+      group: null
+    },
+    {
+      id: "2",
+      name: "Rex Barzee",
+      email: "barzeer@byui.edu",
+      phone: "208-496-3768",
+      imageUrl: "barzeer.jpg",
+      group: null
+    },
+  ];
+}
