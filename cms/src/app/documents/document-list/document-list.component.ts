a3bb55a9b9a4f1de899c469216909ec255424449
Stephen Skiles <stephentskiles@hotmail.com>
2024-10-08 10:45:01 -0600

    Up to dropdown directive

--- cms/src/app/documents/document-list/document-list.component.ts
+++ cms/src/app/documents/document-list/document-list.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
+import { Document } from '../document.model';
 
 @Component({
   selector: 'app-document-list',
@@ -6,5 +7,39 @@ import { Component } from '@angular/core';
   styleUrl: './document-list.component.css'
 })
 export class DocumentListComponent {
+  @Output() selectedDocumentEvent  = new EventEmitter<Document>();
+  documents: Document[] = [
+    {
+      id: "1",
+      name: "Test 1",
+      description: "Description 1",
+      url: "url 1",
+      children: []
+    },
+    {
+      id: "2",
+      name: "Test 2",
+      description: "Description 2",
+      url: "url 2",
+      children: []
+    },
+    {
+      id: "3",
+      name: "Test 3",
+      description: "Description 3",
+      url: "url 3",
+      children: []
+    },
+    {
+      id: "4",
+      name: "Test 4",
+      description: "Description 4",
+      url: "url 4",
+      children: []
+    },
+  ];
 
+  onSelectedDocument(document: Document){
+    this.selectedDocumentEvent.emit(document);
+  }
 }
